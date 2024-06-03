d00aeac04998a41ddac5e95eb3b29e8af064cb3f
leejeongwoo <vulpes54871@gmail.com>
2024-06-03 13:43:07 +0900

    added package dependencies, initialize pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,27 @@
-import "./App.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 
-import { useState } from "react";
-import { useTranslation } from "react-i18next";
-import styled, { CSSProperties } from "styled-components";
+import HomePage from "./pages/home/page";
+import Paths from "./paths";
 
-import ReactLogo from "./assets/react.svg?react";
-
-const ReadTheDocs = styled.p<{
-  $color?: CSSProperties["color"];
-}>`
-  color: ${({ $color }) => $color ?? "red"};
-`;
-
-function App() {
-  const [count, setCount] = useState(0);
-  const { t } = useTranslation("main");
+const App = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route path={Paths.HOME} element={<HomePage />} />
+      </>,
+    ),
+  );
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src="/src/assets/react.svg" className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>귀여운 춘식이</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <ReadTheDocs $color={"blue"}>
-        {t("title")}
-        <ReactLogo width={16} height={16} />
-      </ReadTheDocs>
-    </div>
+    <>
+      <RouterProvider router={router} />
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/paths.ts
@@ -0,0 +1,6 @@
+enum Paths {
+  HOME = "/",
+  WRITE = "/write",
+}
+
+export default Paths;
